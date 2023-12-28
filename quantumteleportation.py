3c203b8f84412a504faf17c281f6a0daa4fa9ba3
shubhamkaushal <shubhamkaushal765@gmail.com>
2023-12-28 18:15:40 +0530

    Quantum Teleportaton Completed!

--- quantumteleportation.py
+++ quantumteleportation.py
@@ -2,12 +2,15 @@ from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, tran
 from qiskit.quantum_info import Statevector
 import numpy as np
 
-def get_bell_state(state='phi+'):
+# SHOTS is the number of times to run the simulation
+# 1 if ideal-local simulation, big number (e.g. 1024) if noisy simulation
+SHOTS = 1
 
-    assert state in ['phi+', 'phi-', 'psi+', 'psi-']
+def get_bell_state(state='phi+', from_circuit=False):
 
-    ##### TODO: add from_circuit to the function argument after completing modifying the circuit.
-    from_circuit = False
+    assert state in ['phi+', 'phi-', 'psi+', 'psi-'], "Unknown state"
+
+    # Predefined bell states
     if not from_circuit:
         if state == 'phi+':
             return Statevector([1./np.sqrt(2), 0, 0, 1./np.sqrt(2)])
@@ -18,19 +21,28 @@ def get_bell_state(state='phi+'):
         elif state == 'psi+':
             return Statevector([0, 1./np.sqrt(2), -1./np.sqrt(2), 0])
 
-    # preparing an entangled state
-    ##### TODO: modify circuit for different bell states
+    # Preparing an entangled state
+    # Caclulating the bell states
     ## circuit
     circuit = QuantumCircuit(2)
+    if state == 'phi+':
+        pass
+    elif state == 'phi-':
+        circuit.x(0)
+    elif state == 'psi+':
+        circuit.x(1)
+    elif state == 'psi-':
+        circuit.x(0)
+        circuit.x(1)
+
     circuit.h(0)
     circuit.cx(0, 1)
 
     ## qubits as state vectors
     svsim = Aer.get_backend("statevector_simulator")
-    job_sv = svsim.run(transpile(circuit, svsim), shots=100)
+    job_sv = svsim.run(transpile(circuit, svsim), shots=SHOTS)
     results = job_sv.result()
     statevector = results.get_statevector()
-    print(statevector)
     return statevector
 
 def teleportation_circuit(q, ab):
@@ -41,11 +53,13 @@ def teleportation_circuit(q, ab):
     qc.initialize(q, [0]) # q
     qc.initialize(ab, [1, 2]) # a, b
 
-    qc.cnot(qr[0], qr[1])
+    qc.cx(qr[0], qr[1])
     qc.h(qr[0])
+    qc.barrier()
 
     qc.measure(qr[0], cr[0])
     qc.measure(qr[1], cr[1])
+    qc.barrier()
 
     qc.cx(qr[1], qr[2])
     qc.cz(qr[0], qr[2])
@@ -54,7 +68,7 @@ def teleportation_circuit(q, ab):
 
 
 if __name__ == "__main__":
-    ab_statevector = get_bell_state(state='phi+')
+    ab_statevector = get_bell_state(state='phi+', from_circuit=True)
 
     # qubit to teleport
     qbit_2_teleport = Statevector([3/5, 4/5])
@@ -67,6 +81,6 @@ if __name__ == "__main__":
     # run simulation
     print(qbit_2_teleport)
     svsim = Aer.get_backend("statevector_simulator")
-    jobs_teleportation = svsim.run(transpile(qc, svsim), shots=100)
+    jobs_teleportation = svsim.run(transpile(qc, svsim), shots=SHOTS)
     results = jobs_teleportation.result()
     print(results.get_statevector())
